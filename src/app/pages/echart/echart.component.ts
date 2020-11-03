9d698d85530bca42e861c4860c18197c0c3aa5c3
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-03 15:37:05 +0900

    chart 예제 값 변경 추가

--- src/app/pages/echart/echart.component.ts
+++ src/app/pages/echart/echart.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-echart',
-  template: `<div echarts [options]="options" class="demo-chart"></div>`,
+  template: `
+    <div echarts [options]="options" class="demo-chart"></div>
+
+    <button (click)="changeOption()">값 변경</button>
+  `,
 })
-export class EchartComponent implements OnInit {
+export class EchartComponent implements AfterViewInit {
   options: any;
   constructor() {}
-  ngOnInit() {
+
+  ngAfterViewInit() {
+    this.options = this.getOption();
+  }
+
+  getOption() {
     const xAxisData = [];
     const data1 = [];
     const data2 = [];
@@ -16,6 +25,46 @@ export class EchartComponent implements OnInit {
       xAxisData.push('category' + i);
       data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
       data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    }
+
+    return {
+      tooltip: {},
+      xAxis: {
+        data: xAxisData,
+        silent: false,
+        splitLine: {
+          show: false,
+        },
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'bar',
+          type: 'bar',
+          data: data1,
+          animationDelay: (idx) => idx * 10,
+        },
+        {
+          name: 'bar2',
+          type: 'bar',
+          data: data2,
+          animationDelay: (idx) => idx * 10 + 100,
+        },
+      ],
+      animationEasing: 'elasticOut',
+      animationDelayUpdate: (idx) => idx * 5,
+    };
+  }
+
+  changeOption() {
+    const xAxisData = [];
+    const data1 = [];
+    const data2 = [];
+
+    for (let i = 0; i < 100; i++) {
+      xAxisData.push('category' + i);
+      data1.push(Math.floor(Math.random()*10));
+      data2.push(Math.floor(Math.random()*10));
     }
 
     this.options = {
