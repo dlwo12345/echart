0342c4bcf498ca796f2015de9e03aa7f1b581d17
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-03 15:14:21 +0900

    공통모듈 설정

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [SharedModule, RouterModule.forChild(routes)],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
