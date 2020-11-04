92f2650a0b4a0ddab08c2b3fddfe70fe89d3609a
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-04 11:43:51 +0900

    pie-chart 예제 추가

--- src/app/pages/echart/pie-chart/pie-chart.component.ts
+++ src/app/pages/echart/pie-chart/pie-chart.component.ts
@@ -1,12 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { graphic } from 'echarts';
 
 @Component({
   selector: 'app-pie-chart',
-  template: ``,
+  template: ` <div echarts [options]="options" class="demo-chart"></div>
+    <button (click)="changeLabel()">changeLabel</button>`,
 })
 export class PieChartComponent implements OnInit {
+  options: any;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.options = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{b} : {d}% ({c}) ',
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: '65%',
+          center: ['50%', '50%'],
+          selectedMode: 'single',
+          label: {
+            position: 'inside',
+            formatter: '{b}\n{d}%',
+            fontSize: 10,
+            color: '#fff',
+          },
+          data: [
+            { value: 154, name: '원리금', itemStyle: { color: '#6ea6e1' } },
+            { value: 535, name: '세금', itemStyle: { color: '#458fdc' } },
+            { value: 510, name: '공과금', itemStyle: { color: '#175ca5' } },
+            { value: 634, name: '운영비', itemStyle: { color: '#05407e' } },
+            { value: 735, name: '기타', itemStyle: { color: '#0f3256' } },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            },
+          },
+        },
+      ],
+    };
+  }
+
+  changeLabel() {
+    const newOption = { ...this.options };
+
+    newOption.series[0].label.position =
+      newOption.series[0].label.position === 'inside' ? 'outside' : 'inside';
+
+    newOption.series[0].label.color =
+      newOption.series[0].label.position === 'inside' ? '#fff' : 'auto';
+
+    this.options = newOption;
+  }
 }
