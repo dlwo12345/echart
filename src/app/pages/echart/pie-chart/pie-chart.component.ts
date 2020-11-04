964445303b635c75bc6383e0011ecdd683044369
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-04 15:28:20 +0900

    pie-chart 데이터 없을때 dummy 추가

--- src/app/pages/echart/pie-chart/pie-chart.component.ts
+++ src/app/pages/echart/pie-chart/pie-chart.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pie-chart',
-  template: ` <div echarts [options]="options" class="demo-chart"></div>
-    <button (click)="changeLabel()">changeLabel</button>`,
+  template: `
+    <div echarts [options]="options" class="demo-chart"></div>
+    <button (click)="changeLabel()">changeLabel</button>
+    <button (click)="changeData()">changeData</button>
+  `,
 })
 export class PieChartComponent implements OnInit {
   options: any;
@@ -28,6 +31,7 @@ export class PieChartComponent implements OnInit {
             color: '#fff',
           },
           data: [
+            // { value: 100, itemStyle: { color: 'rgba(80, 80, 80, 0.5)' } }, // 데이터없을때 케이스
             { value: 154, name: '원리금', itemStyle: { color: '#6ea6e1' } },
             { value: 535, name: '세금', itemStyle: { color: '#458fdc' } },
             { value: 510, name: '공과금', itemStyle: { color: '#175ca5' } },
@@ -54,6 +58,31 @@ export class PieChartComponent implements OnInit {
 
     newOption.series[0].label.color =
       newOption.series[0].label.position === 'inside' ? '#fff' : 'auto';
+
+    this.options = newOption;
+  }
+
+  changeData() {
+    const newOption = { ...this.options };
+
+    const nullDataOption = [
+      {
+        value: 100,
+        itemStyle: { color: 'rgba(80, 80, 80, 0.5)' },
+      },
+    ];
+    const originalDataOption = [
+      { value: 154, name: '원리금', itemStyle: { color: '#6ea6e1' } },
+      { value: 535, name: '세금', itemStyle: { color: '#458fdc' } },
+      { value: 510, name: '공과금', itemStyle: { color: '#175ca5' } },
+      { value: 634, name: '운영비', itemStyle: { color: '#05407e' } },
+      { value: 735, name: '기타', itemStyle: { color: '#0f3256' } },
+    ];
+
+    newOption.series[0].data =
+      newOption.series[0].data.length === 1
+        ? originalDataOption
+        : nullDataOption;
 
     this.options = newOption;
   }
